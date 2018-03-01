81a13786b965986a46ef73a546575b1f6228dcfd
rranauro <ron@ranauro.net>
2018-03-01 16:39:37 -0500

    suppress log in create

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ ConnectWrapper.prototype.create = function( collection ) {
 				
 				// copy docs 1000 at a time
 				async.eachLimit(_.range(0, req.body.length, 10000), 1, function(start, go) {
-					console.log('[create] info:', collection, start, req.body.length);
+					if (req.body.length > 100) console.log('[create] info:', collection, start, req.body.length);
 					self._db.collection( collection ).insertMany(req.body.slice(start, start+10000), options, go);
 				}, next);
 				
